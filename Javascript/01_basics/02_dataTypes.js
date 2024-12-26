c5fcbb5989fef8c8cd4dad248b9070b53a88b29a
Sanjana <128401489+SanjanaChaurasia@users.noreply.github.com>
2024-12-26 19:04:36 +0000

    datatypes

---
+++ Javascript/01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all code as newer version
+
+// alert( 3 + 3 ); we are using node.js not the browser
+
+console.log( 3
+    +
+    3 ); //code readability should be high
+
+console.log("Sanjana");
+
+
+let name = "Sanjana" //string data type
+let age = 24 //integer data type
+let isLoggedIn = false
+
+
+// number => 2 to power 53 - range of number
+// bigint => for long numbers
+// string => "text is string"
+// boolean => true/false
+// null => standalone value
+// undefined => where variable is declared but value is not provided
+// symbol => to find uniqueness 
+
+// object
+
+console.log(name +"-"+ typeof name);
+console.log(age +"-"+ typeof age);
+console.log(isLoggedIn +"-"+ typeof isLoggedIn);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+

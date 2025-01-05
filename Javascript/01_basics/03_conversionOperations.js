0ca353ca8bcff943382f7c3bfd780a2be27dffa2
Sanjana <128401489+SanjanaChaurasia@users.noreply.github.com>
2025-01-05 17:38:39 +0000

    conversion

---
+++ Javascript/01_basics/03_conversionOperations.js
@@ -0,0 +1,108 @@
+let score = 50
+let test = "40"
+let work = "20abc"
+let space = null
+let milk = undefined
+let ans = true
+let answr = false
+let first = "Sanjana"
+
+console.log(score);
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+console.log("----------------");
+
+console.log(test);
+console.log(typeof test);
+let valueInNumb = Number(test)
+console.log(typeof valueInNumb);
+console.log(valueInNumb);
+
+console.log("----------------");
+
+console.log(work);
+console.log(typeof work);
+let valueInNum = Number(work)
+console.log(typeof valueInNum);
+console.log(valueInNum);
+
+console.log("----------------");
+
+console.log(space);
+console.log(typeof space);
+let valueNum = Number(space)
+console.log(typeof valueNum);
+console.log(valueNum);
+
+console.log("----------------");
+
+console.log(milk);
+console.log(typeof milk);
+let valueIn = Number(milk)
+console.log(typeof valueIn);
+console.log(valueIn);
+
+console.log("----------------");
+
+console.log(ans);
+console.log(typeof ans);
+let valueInNumbr = Number(ans)
+console.log(typeof valueInNumbr);
+console.log(valueInNumbr);
+
+console.log("----------------");
+
+console.log(answr);
+console.log(typeof answr);
+let valInNumber = Number(answr)
+console.log(typeof valInNumber);
+console.log(valInNumber);
+
+console.log("----------------");
+
+console.log(first);
+console.log(typeof first);
+let valNum = Number(first)
+console.log(typeof valNum);
+console.log(valNum);
+
+
+// Notes
+// "40" => 40
+// "20abc" => NaN
+// true => 1
+// false => 0
+console.log("----------------");
+let isLoggedIn = 1
+console.log(isLoggedIn);
+console.log(typeof isLoggedIn);
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
+
+console.log("----------------");
+let isLoggedOut = 0
+console.log(isLoggedOut);
+console.log(typeof isLoggedOut);
+let booleanIsLoggedOut = Boolean(isLoggedOut)
+console.log(typeof booleanIsLoggedOut);
+console.log(booleanIsLoggedOut);
+
+console.log("----------------");
+let isLogged = ""
+console.log(isLogged);
+console.log(typeof isLogged);
+let booleanIsLogged = Boolean(isLogged)
+console.log(typeof booleanIsLogged);
+console.log(booleanIsLogged);
+
+console.log("----------------");
+let logged = "Sanjana"
+console.log(logged);
+console.log(typeof logged);
+let booleanLogged = Boolean(logged)
+console.log(typeof booleanLogged);
+console.log(booleanLogged);
